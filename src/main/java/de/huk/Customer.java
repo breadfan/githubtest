5b4939f00f526c427d8de9d819f2103b1ec41319
Chris Wewerka <breadfan@gmx.de>
2012-12-06 11:37:07 +0100

    Customer Klasse angelegt

---
+++ src/main/java/de/huk/Customer.java
@@ -0,0 +1,53 @@
+package de.huk;
+
+/**
+ * @author Chris Wewerka
+ */
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String houseNumber;
+    private String city;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
